91ba4a135641c2cdcfbf754a0db6da2b2e1f0bb8
odenilson marques <odmarquesgm@gmail.com>
2024-06-15 13:20:33 -0300

    alteracao nas cores e nas imagens

---
+++ front-page.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+<main>
+    <section class="hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <h1>Origamis Únicos</h1>
+                    <h2 class="mt-5">Crie lembranças inesquecíveis com origamis personalizados feitos à mão com amor e carinho.</h2>
+                    <a href="#" class="btn  mt-5">Conheça nossa Loja</a>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 text-center" >
+                    <img src="<?= bloginfo('stylesheet_directory'); ?>/assets/img/teste.png" class="img-fluid " alt="origami personalizado">
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
--- header.php
+++ header.php
@@ -1,9 +1,40 @@
 <!DOCTYPE html>
-<html <?php language_attributes();?>>
+<html <?php language_attributes(); ?>>
+
 <head>
-    <meta charset="<?php bloginfo('charset');?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Origami</title>
-    <?php wp_head();?>
+    <?php wp_head(); ?>
 </head>
-<body  <?php body_class();?>>
+
+<body <?php body_class(); ?>>
+    <header class="custom-header">
+        <nav class="navbar navbar-expand-sm navbar-dark" style="background-color:#AF47D2">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    Origami
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <nav class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+                    <ul class="navbar-nav">
+                        
+                        <li class="nav-item me-3">
+                            <a class="nav-link" href="#">catálogo</a>
+                        </li>
+                        <li class="nav-item me-3">
+                            <a class="nav-link" href="#pricing">Videos</a>
+                        </li> 
+                        <li class="nav-item me-3">
+                            <a class="nav-link" href="#pricing">Sobre nós</a>
+                        </li>
+                        <li class="nav-item me-3">
+                            <a class="nav-link" href="#contact">Converse com a gente</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </nav>
+    </header>
